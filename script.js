1deca1855f9832801c3609370cb50ce5d4503091
Matej-Basta <71135305+Matej-Basta@users.noreply.github.com>
2022-01-30 19:19:47 +0100

    Simple working with arrays done

---
+++ script.js
@@ -0,0 +1,13 @@
+const hours = [9, 12, 8, 3, 6, 12, 10];
+
+console.log(`I worked about ${hours[6]} hours last Sunday.`);
+
+const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
+
+days.push("Saturday", "Sunday");
+
+console.log(days[0]);
+console.log(days[2]);
+console.log(days[5]);
+
+console.log(days.length);
